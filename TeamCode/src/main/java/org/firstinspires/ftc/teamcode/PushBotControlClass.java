275d6c0d728022e431adb13d9338f63c6cfdb064
Jake Franklin <matthew3j02@gmail.com>
2017-09-11 20:05:49 -0500

    adding push bot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushBotControlClass.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Fam on 9/9/2017.
+ */
+
+
+@TeleOp(name = "PushBotTest")
+
+public class PushBotControlClass extends OpMode
+
+{
+
+
+    DcMotor motor1;
+    DcMotor motor2;
+    DcMotor motor3;
+    Servo servo1;
+    Servo servo2;
+
+
+    public void init()
+
+    {
+        motor1 = hardwareMap.dcMotor.get("Rightside_Motor");
+        motor2 = hardwareMap.dcMotor.get("Leftside_Motor");
+        motor3 = hardwareMap.dcMotor.get("Arm_Motor");
+
+    }
+
+
+    public void loop()
+
+    {
+
+        if(gamepad1.left_stick_x == 1)
+
+        {
+            motor2.setPower(100);
+            motor1.setPower(100);
+            motor3.setPower(0);
+        }
+
+        else if(gamepad1.left_stick_x == -1)
+
+        {
+            motor1.setPower(-100);
+            motor2.setPower(-100);
+            motor3.setPower(0);
+        }
+
+        else if(gamepad1.dpad_up == true)
+        {
+            motor3.setPower(100);
+        }
+
+        else if(gamepad1.dpad_down == true)
+        {
+            motor3.setPower(-100);
+        }
+
+        else
+
+        {
+            motor1.setPower(gamepad1.right_stick_y);
+            motor2.setPower(-gamepad1.right_stick_y);
+            motor3.setPower(0);
+        }
+
+
+    }
+
+}
