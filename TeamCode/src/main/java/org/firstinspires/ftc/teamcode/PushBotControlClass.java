09d74b8d1f399d8e29e90ef13875ec99fbfdfd5f
Jake Franklin <matthew3j02@gmail.com>
2017-09-16 17:38:21 -0500

    deleted servo lines we are not using

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushBotControlClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushBotControlClass.java
@@ -5,22 +5,20 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorREVColorDistance;
+
 /**
  * Created by Fam on 9/9/2017.
  */
 
-
 @TeleOp(name = "PushBotTest")
+
 public class PushBotControlClass extends OpMode
 
 {
-
-
     DcMotor motor1;
     DcMotor motor2;
     DcMotor motor3;
-    Servo servo1;
-    Servo servo2;
 
 
     public void init()
@@ -29,16 +27,12 @@ public class PushBotControlClass extends OpMode
         motor1 = hardwareMap.dcMotor.get("Rightside_Motor");
         motor2 = hardwareMap.dcMotor.get("Leftside_Motor");
         motor3 = hardwareMap.dcMotor.get("Arm_Motor");
-
     }
-
 
     public void loop()
 
     {
-
         if(gamepad1.left_stick_x == 1)
-
         {
             motor2.setPower(100);
             motor1.setPower(100);
@@ -46,7 +40,6 @@ public class PushBotControlClass extends OpMode
         }
 
         else if(gamepad1.left_stick_x == -1)
-
         {
             motor1.setPower(-100);
             motor2.setPower(-100);
@@ -64,14 +57,10 @@ public class PushBotControlClass extends OpMode
         }
 
         else
-
         {
             motor1.setPower(gamepad1.right_stick_y);
             motor2.setPower(-gamepad1.right_stick_y);
             motor3.setPower(0);
         }
-
-
     }
-
 }
