2bba88bfeddeb1fd6869dce93c2374edd26f1392
Jake Franklin <matthew3j02@gmail.com>
2017-11-10 22:44:25 -0600

    Op modes for Teleop and Autonomous

--- TeamCode/src/main/java/AutonomousQuadrant4Blue.java
+++ TeamCode/src/main/java/AutonomousQuadrant4Blue.java
@@ -29,36 +29,34 @@ public class AutonomousQuadrant4Blue extends LinearOpMode
         sensorColor = hardwareMap.get(ColorSensor.class, "sensor_color_distance");
         sensorDistance = hardwareMap.get(DistanceSensor.class, "sensor_color_distance");
 
+        waitForStart();
+
         servo1.setPosition(0);
 
         sleep(1000);
 
-        if (sensorColor.red() > sensorColor.blue() && opModeIsActive()) {
-            motor1.setPower(.30);
-            motor2.setPower(-.30);
+        if (sensorColor.red() > sensorColor.blue()&& opModeIsActive()) {
+            motor2.setPower(-.30); //backwards
 
-            sleep(250);
+            sleep(350);
 
-            motor1.setPower(0);
             motor2.setPower(0);
 
             servo1.setPosition(.7);
 
             sleep(1000);
 
-            motor1.setPower(-.40);
-            motor2.setPower(.40);
+            motor2.setPower(.30); //forwards
 
-            sleep(350);
+            sleep(250);
 
 
         }
 
-        if (sensorColor.red() < sensorColor.blue() && opModeIsActive()) {
-            motor1.setPower(-.30);
-            motor2.setPower(.30);
+        if (sensorColor.red() < sensorColor.blue()&& opModeIsActive()) {
+            motor1.setPower(.30);//forwards
 
-            sleep(250);
+            sleep(350);
 
             motor1.setPower(0);
             motor2.setPower(0);
@@ -67,10 +65,9 @@ public class AutonomousQuadrant4Blue extends LinearOpMode
 
             sleep(1000);
 
-            motor1.setPower(.40);
-            motor2.setPower(-.40);
+            motor1.setPower(-.30);
 
-            sleep(350);
+            sleep(250);
         }
 
         motor1.setPower(0);
