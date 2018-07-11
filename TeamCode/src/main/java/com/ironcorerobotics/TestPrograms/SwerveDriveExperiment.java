b1b2e8352c1df9fdaf734d73d1ba7f62e7ded2d8
Jake Franklin <matthew3j02@gmail.com>
2018-07-11 13:45:19 -0500

    Working Swerve Drive Prototype code (goes with Mark 3)

--- TeamCode/src/main/java/com/ironcorerobotics/TestPrograms/SwerveDriveExperiment.java
+++ TeamCode/src/main/java/com/ironcorerobotics/TestPrograms/SwerveDriveExperiment.java
@@ -1,5 +1,6 @@
 package com.ironcorerobotics.TestPrograms;
 
+import com.ironcorerobotics.ControlClasses.MotorControl;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -23,6 +24,9 @@ public class SwerveDriveExperiment extends OpMode {
     DcMotor motor1;
     DcMotor motor2;
     DcMotor motor3;
+    MotorControl ControlMotor1 = new MotorControl(1.00, 2, 5);
+    MotorControl ControlMotor2 = new MotorControl(-1.00, 2, 5);
+    MotorControl ControlMotor3 = new MotorControl(1.00,2,5);
 
 
         public void init()
@@ -60,38 +64,38 @@ public class SwerveDriveExperiment extends OpMode {
             if(x > 0 && y < 0)//topright
             {
                 double cordfranction = x / y;
-                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction))))*ASymbol)/100;
+                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction)))+90)*ASymbol)/100;
             }
             if(x > 0 && y > 0)//bottomright
             {
                 double cordfranction = y / x;
-                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction)))+90)*ASymbol)/100;
+                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction)))+180)*ASymbol)/100;
             }
             if(x < 0 && y > 0)//bottomleft
             {
                 double cordfranction = x / y;
-                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction)))+180)*ASymbol)/100;
+                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction)))+270)*ASymbol)/100;
             }
             if(x < 0 && y < 0)//topleft
             {
                 double cordfranction = y / x;
-                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction)))+270)*ASymbol)/100;
+                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction))))*ASymbol)/100;
             }
             if(x == 0 && y == -1.00 || gamepad1.dpad_up) //Forward
             {
-                returnthis = 0;
+                returnthis = ((90*ASymbol)/100);
             }
             if(x == 1.00 && y == 0 || gamepad1.dpad_right) //Right
             {
-                returnthis = ((90*ASymbol)/100);
+                returnthis = ((180*ASymbol)/100);
             }
             if(x == 0 && y == 1.00 || gamepad1.dpad_down) //Backwards
             {
-                returnthis = ((180*ASymbol)/100);
+                returnthis = ((270*ASymbol)/100);
             }
             if(x == -1.00 && y ==0 || gamepad1.dpad_left) //Left
             {
-                returnthis = ((270*ASymbol)/100);
+                returnthis = 0;
             }
             return returnthis;
         }
@@ -106,15 +110,61 @@ public class SwerveDriveExperiment extends OpMode {
                 motorRun=false;
             }
             if(motorRun==true) {
-                motor1.setPower(1.00);
-                motor2.setPower(1.00);
-                motor3.setPower(1.00);
+                motor1.setPower(ControlMotor1.getControlledSpeed());
+                motor2.setPower(ControlMotor2.getControlledSpeed());
+                motor3.setPower(ControlMotor3.getControlledSpeed());
             }
             if(motorRun==false) {
                 motor1.setPower(0);
                 motor2.setPower(0);
                 motor3.setPower(0);
-            }
+            }if(gamepad1.y) {
+            ControlMotor1.setThirdButton(true);
+            ControlMotor2.setThirdButton(true);
+            ControlMotor3.setThirdButton(true);
+            ControlMotor1.setSecondButton(false);
+            ControlMotor2.setSecondButton(false);
+            ControlMotor3.setSecondButton(false);
+            ControlMotor1.setFirstButton(false);
+            ControlMotor2.setFirstButton(false);
+            ControlMotor3.setFirstButton(false);
+
+        } else if(gamepad1.right_bumper) {
+            ControlMotor1.setThirdButton(false);
+            ControlMotor2.setThirdButton(false);
+            ControlMotor3.setThirdButton(false);
+            ControlMotor1.setSecondButton(true);
+            ControlMotor2.setSecondButton(true);
+            ControlMotor3.setSecondButton(true);
+            ControlMotor1.setFirstButton(false);
+            ControlMotor2.setFirstButton(false);
+            ControlMotor3.setFirstButton(false);
+        }else if (gamepad1.left_bumper) {
+            ControlMotor1.setThirdButton(false);
+            ControlMotor2.setThirdButton(false);
+            ControlMotor3.setThirdButton(false);
+            ControlMotor1.setSecondButton(false);
+            ControlMotor2.setSecondButton(false);
+            ControlMotor3.setSecondButton(false);
+            ControlMotor1.setFirstButton(true);
+            ControlMotor2.setFirstButton(true);
+            ControlMotor3.setFirstButton(true);
+        }else if (gamepad1.right_stick_x > 0){//turning
+            motor1.setPower(-ControlMotor1.getControlledSpeed());
+            motor2.setPower(ControlMotor2.getControlledSpeed());
+            motor3.setPower(-ControlMotor3.getControlledSpeed());
+            testServo.setPosition(0);
+            testServo2.setPosition(0);
+            testServo3.setPosition(0);
+        }else if (gamepad1.right_stick_x < 0){
+            motor1.setPower(ControlMotor1.getControlledSpeed());
+            motor2.setPower(-ControlMotor2.getControlledSpeed());
+            motor3.setPower(ControlMotor3.getControlledSpeed());
+            testServo.setPosition(0);
+            testServo2.setPosition(0);
+            testServo3.setPosition(0);
+        }
+
         }
 
     }
