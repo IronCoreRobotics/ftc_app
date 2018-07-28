8bd7d3f9589e6911728913cdcdafac8a294df5e7
Jake Franklin <matthew3j02@gmail.com>
2018-07-28 15:19:15 -0500

    Swerve Drive Upgrade

--- TeamCode/src/main/java/com/ironcorerobotics/TestPrograms/SwerveDriveExperiment.java
+++ TeamCode/src/main/java/com/ironcorerobotics/TestPrograms/SwerveDriveExperiment.java
@@ -22,8 +22,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class SwerveDriveExperiment extends OpMode {
 
-        //Declare any global variables here
-        //Build any objects like DC motors and such here
+    //Declare any global variables here
+    //Build any objects like DC motors and such here
     double ASymbol = .2777777778;
     Servo testServo;
     Servo testServo2;
@@ -33,109 +33,125 @@ public class SwerveDriveExperiment extends OpMode {
     DcMotor motor3;
     MotorControl ControlMotor1 = new MotorControl(1.00, 2, 5);
     MotorControl ControlMotor2 = new MotorControl(-1.00, 2, 5);
-    MotorControl ControlMotor3 = new MotorControl(1.00,2,5);
+    MotorControl ControlMotor3 = new MotorControl(1.00, 2, 5);
     BNO055IMU imu;
     Orientation angles;
+    boolean runGyroThread;
 
 
-        public void init()
+    public void init()
 
+    {
+        //Put your initialization code here
+        //Hardware map everything
+        testServo = hardwareMap.servo.get("direction_servo");
+        testServo2 = hardwareMap.servo.get("servo2");
+        testServo3 = hardwareMap.servo.get("servo3");
+        motor1 = hardwareMap.dcMotor.get("drive_motor");
+        motor2 = hardwareMap.dcMotor.get("motor2");
+        motor3 = hardwareMap.dcMotor.get("motor3");
+        testServo.setDirection(Servo.Direction.REVERSE);
+        testServo2.setDirection(Servo.Direction.REVERSE);
+        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        ControlMotor1.setThirdButton(true);
+        ControlMotor2.setThirdButton(true);
+        ControlMotor3.setThirdButton(true);
+        ControlMotor1.setSecondButton(false);
+        ControlMotor2.setSecondButton(false);
+        ControlMotor3.setSecondButton(false);
+        ControlMotor1.setFirstButton(false);
+        ControlMotor2.setFirstButton(false);
+        ControlMotor3.setFirstButton(false);
+
+    }
+
+    public void loop()
+
+    {
+        //Put your main code here
+        t1.isDaemon();
+        t1.start();
+        testServo.setPosition(getCaculatedServoPosition(gamepad1.left_stick_x, gamepad1.left_stick_y));
+        testServo2.setPosition(getCaculatedServoPosition(gamepad1.left_stick_x, gamepad1.left_stick_y));
+        testServo3.setPosition(getCaculatedServoPosition(gamepad1.left_stick_x, gamepad1.left_stick_y));
+        telemetry.addData("robot position", getUpdatedIMUHeading());
+        telemetry.update();
+        controlMotors();
+        if (gamepad1.dpad_left || gamepad1.dpad_down || gamepad1.dpad_up || gamepad1.dpad_right){
+            t1.run();
+    }
+    }
+
+    public double getCaculatedServoPosition(double x, double y) {
+        double returnthis = 0;
+        if (x > 0 && y < 0)//topright
         {
-            //Put your initialization code here
-            //Hardware map everything
-            testServo = hardwareMap.servo.get("direction_servo");
-            testServo2 = hardwareMap.servo.get("servo2");
-            testServo3 = hardwareMap.servo.get("servo3");
-            motor1 = hardwareMap.dcMotor.get("drive_motor");
-            motor2 = hardwareMap.dcMotor.get("motor2");
-            motor3 = hardwareMap.dcMotor.get("motor3");
-            testServo.setDirection(Servo.Direction.REVERSE);
-            testServo2.setDirection(Servo.Direction.REVERSE);
-            motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            motor3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-            parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-            parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-            parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
-            parameters.loggingEnabled      = true;
-            parameters.loggingTag          = "IMU";
-            parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-
-            imu = hardwareMap.get(BNO055IMU.class, "imu");
-            imu.initialize(parameters);
-
+            double cordfranction = x / y;
+            returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction))) + 90) * ASymbol) / 100;
         }
-
-        public void loop()
-
+        if (x > 0 && y > 0)//bottomright
         {
-            //Put your main code here
-            testServo.setPosition(getCaculatedServoPosition(gamepad1.left_stick_x, gamepad1.left_stick_y));
-            testServo2.setPosition(getCaculatedServoPosition(gamepad1.left_stick_x, gamepad1.left_stick_y));
-            testServo3.setPosition(getCaculatedServoPosition(gamepad1.left_stick_x, gamepad1.left_stick_y));
-            //telemetry.addData("servo position", getCaculatedServoPosition(gamepad1.left_stick_x, gamepad1.left_stick_y));
-            telemetry.addData("robot position", getUpdatedIMUHeading());
-            telemetry.update();
-            controlMotors();
+            double cordfranction = y / x;
+            returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction))) + 180) * ASymbol) / 100;
         }
-
-        public double getCaculatedServoPosition(double x, double y)
+        if (x < 0 && y > 0)//bottomleft
         {
-            double returnthis = 0;
-            if(x > 0 && y < 0)//topright
-            {
-                double cordfranction = x / y;
-                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction)))+90)*ASymbol)/100;
-            }
-            if(x > 0 && y > 0)//bottomright
-            {
-                double cordfranction = y / x;
-                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction)))+180)*ASymbol)/100;
-            }
-            if(x < 0 && y > 0)//bottomleft
-            {
-                double cordfranction = x / y;
-                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction)))+270)*ASymbol)/100;
-            }
-            if(x < 0 && y < 0)//topleft
-            {
-                double cordfranction = y / x;
-                returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction))))*ASymbol)/100;
-            }
-            if(x == 0 && y == -1.00 || gamepad1.dpad_up) //Forward
-            {
-                returnthis = ((90*ASymbol)/100);
-            }
-            if(x == 1.00 && y == 0 || gamepad1.dpad_right) //Right
-            {
-                returnthis = ((180*ASymbol)/100);
-            }
-            if(x == 0 && y == 1.00 || gamepad1.dpad_down) //Backwards
-            {
-                returnthis = ((270*ASymbol)/100);
-            }
-            if(x == -1.00 && y ==0 || gamepad1.dpad_left) //Left
-            {
-                returnthis = 0;
-            }
-            return returnthis;
+            double cordfranction = x / y;
+            returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction))) + 270) * ASymbol) / 100;
         }
-
-        public void controlMotors()
+        if (x < 0 && y < 0)//topleft
         {
-            boolean motorRun = false;
-            if(gamepad1.a) {
-                motorRun = true;
-            }
-            if(motorRun==true) {motor1.setPower(ControlMotor1.getControlledSpeed());
-                motor2.setPower(ControlMotor2.getControlledSpeed());
-                motor3.setPower(ControlMotor3.getControlledSpeed());}
-            if(motorRun==false) {
-                motor1.setPower(0);
-                motor2.setPower(0);
-                motor3.setPower(0);
-            }if(gamepad1.y) {
+            double cordfranction = y / x;
+            returnthis = ((Math.abs(Math.toDegrees(Math.atan(cordfranction)))) * ASymbol) / 100;
+        }
+        if (x == 0 && y == -1.00 || gamepad1.y) //Forward
+        {
+            returnthis = ((90 * ASymbol) / 100);
+        }
+        if (x == 1.00 && y == 0 || gamepad1.b) //Right
+        {
+            returnthis = ((180 * ASymbol) / 100);
+        }
+        if (x == 0 && y == 1.00 || gamepad1.a) //Backwards
+        {
+            returnthis = ((270 * ASymbol) / 100);
+        }
+        if (x == -1.00 && y == 0 || gamepad1.x) //Left
+        {
+            returnthis = 0;
+        }
+        return returnthis;
+    }
+
+    public void controlMotors() {
+        boolean motorRun = false;
+        if (gamepad1.right_trigger > 0) {
+            motorRun = true;
+            runGyroThread = false;
+        }
+        if (motorRun == true) {
+            motor1.setPower(ControlMotor1.getControlledSpeed());
+            motor2.setPower(ControlMotor2.getControlledSpeed());
+            motor3.setPower(ControlMotor3.getControlledSpeed());
+        }
+        if (motorRun == false) {
+            motor1.setPower(0);
+            motor2.setPower(0);
+            motor3.setPower(0);
+        }
+        if (gamepad1.right_bumper) {
             ControlMotor1.setThirdButton(true);
             ControlMotor2.setThirdButton(true);
             ControlMotor3.setThirdButton(true);
@@ -146,7 +162,7 @@ public class SwerveDriveExperiment extends OpMode {
             ControlMotor2.setFirstButton(false);
             ControlMotor3.setFirstButton(false);
 
-        } else if(gamepad1.right_bumper) {
+        } else if (gamepad1.left_bumper) {
             ControlMotor1.setThirdButton(false);
             ControlMotor2.setThirdButton(false);
             ControlMotor3.setThirdButton(false);
@@ -156,7 +172,7 @@ public class SwerveDriveExperiment extends OpMode {
             ControlMotor1.setFirstButton(false);
             ControlMotor2.setFirstButton(false);
             ControlMotor3.setFirstButton(false);
-        }else if (gamepad1.left_bumper) {
+        } else if (gamepad1.left_trigger > 0) {
             ControlMotor1.setThirdButton(false);
             ControlMotor2.setThirdButton(false);
             ControlMotor3.setThirdButton(false);
@@ -166,14 +182,14 @@ public class SwerveDriveExperiment extends OpMode {
             ControlMotor1.setFirstButton(true);
             ControlMotor2.setFirstButton(true);
             ControlMotor3.setFirstButton(true);
-        }else if (gamepad1.right_stick_x < 0){//turning//left
+        } else if (gamepad1.right_stick_x < 0) {//turning//left
             motor1.setPower(-ControlMotor1.getControlledSpeed());
             motor2.setPower(ControlMotor2.getControlledSpeed());
             motor3.setPower(-ControlMotor3.getControlledSpeed());
             testServo.setPosition(0);
             testServo2.setPosition(0);
             testServo3.setPosition(0);
-        }else if (gamepad1.right_stick_x > 0){//right
+        } else if (gamepad1.right_stick_x > 0) {//right
             motor1.setPower(ControlMotor1.getControlledSpeed());
             motor2.setPower(-ControlMotor2.getControlledSpeed());
             motor3.setPower(ControlMotor3.getControlledSpeed());
@@ -181,32 +197,88 @@ public class SwerveDriveExperiment extends OpMode {
             testServo2.setPosition(0);
             testServo3.setPosition(0);
         }
-//        else if (gamepad1.b){
-//            boolean run = true;
-//            telemetry.addLine("I AM IN THE LOOP");
-//            while (run == true) {
-//                motor1.setPower(ControlMotor1.getControlledSpeed());
-//                motor2.setPower(-ControlMotor2.getControlledSpeed());
-//                motor3.setPower(ControlMotor3.getControlledSpeed());
-//                if(getUpdatedIMUHeading() == 90){
-//                    run = false;
-//                }
-//            }
-//        }
 
-        }
 
-    public double getUpdatedIMUHeading(){
+    }
+
+    public double getUpdatedIMUHeading() {
         double convertedHeading = 0;
-        angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         double currentOrientation = AngleUnit.DEGREES.normalize(angles.firstAngle);
         telemetry.update();
-        if(currentOrientation > 0){
-            convertedHeading =  360 - currentOrientation;
-        }else if(currentOrientation < 0){
+        if (currentOrientation > 0) {
+            convertedHeading = 360 - currentOrientation;
+        } else if (currentOrientation < 0) {
             convertedHeading = Math.abs(currentOrientation);
         }
         return convertedHeading;
     }
 
-    }
+    Thread t1 = new Thread() {
+        public void run() {
+
+            if (gamepad1.dpad_right) {
+                boolean runGyroThread = true;
+                while (runGyroThread == true) {
+                    motor1.setPower(ControlMotor1.getControlledSpeed());
+                    motor2.setPower(-ControlMotor2.getControlledSpeed());
+                    motor3.setPower(ControlMotor3.getControlledSpeed());
+                    if (getUpdatedIMUHeading() > 70 && getUpdatedIMUHeading() < 110) {
+                        runGyroThread = false;
+                        t1.interrupt();
+                        telemetry.addLine("I AM IN THE IF");
+                        break;
+                    }
+                }
+            }
+            else if (gamepad1.dpad_up) {
+                boolean runGyroThread = true;
+                while (runGyroThread == true) {
+                    motor1.setPower(ControlMotor1.getControlledSpeed());
+                    motor2.setPower(-ControlMotor2.getControlledSpeed());
+                    motor3.setPower(ControlMotor3.getControlledSpeed());
+                    if (getUpdatedIMUHeading() > 340 && getUpdatedIMUHeading() < 360) {
+                        runGyroThread = false;
+                        t1.interrupt();
+                        telemetry.addLine("I AM IN THE IF");
+                        break;
+                    }
+                }
+            }
+           else if (gamepad1.dpad_down) {
+                boolean runGyroThread = true;
+                while (runGyroThread == true) {
+                    motor1.setPower(ControlMotor1.getControlledSpeed());
+                    motor2.setPower(-ControlMotor2.getControlledSpeed());
+                    motor3.setPower(ControlMotor3.getControlledSpeed());
+                    if (getUpdatedIMUHeading() > 160 && getUpdatedIMUHeading() < 200) {
+                        runGyroThread = false;
+                        t1.interrupt();
+                        telemetry.addLine("I AM IN THE IF");
+                        break;
+                    }
+                }
+            }
+            else if (gamepad1.dpad_left) {
+                boolean runGyroThread = true;
+                while (runGyroThread == true) {
+                    motor1.setPower(ControlMotor1.getControlledSpeed());
+                    motor2.setPower(-ControlMotor2.getControlledSpeed());
+                    motor3.setPower(ControlMotor3.getControlledSpeed());
+                    if (getUpdatedIMUHeading() > 250 && getUpdatedIMUHeading() < 290) {
+                        runGyroThread = false;
+                        t1.interrupt();
+                        telemetry.addLine("I AM IN THE IF");
+                        break;
+                    }
+                }
+            }
+        }
+
+    };
+
+}
+
+
+
+
